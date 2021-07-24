32c853ce4755a3f80c726a62b608c4f5d3a91a2e
kiun <mm11game@gmail.com>
2021-07-24 19:09:42 +0900

    6 time test

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,18 +3,20 @@ dotenv.config();
 
 const config = {
   development: {
-    host: process.env.DBHOST,
-    username: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: "microft",
     dialect: "mysql",
+    port: 3306,
   },
   production: {
-    host: process.env.DBHOST,
-    username: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
+    host: process.env.DBH_OST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: "microft",
     dialect: "mysql",
+    port: 3306,
   },
 };
 
